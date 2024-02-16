78c2386063bd2e8af8fd6a1bf1254e8e5cc61614
letyresina <leticiaresina@gmail.com>
2024-02-16 11:24:12 -0300

    exercício 2

--- src/br/com/fiap/Exercicio2.java
+++ src/br/com/fiap/Exercicio2.java
@@ -13,6 +13,17 @@ public class Exercicio2 {
 		// Programa principal
 		
 		try {
+			scan = new Scanner(System.in); // instanciando o objeto
+			
+			System.out.println("Em que ano estamos atualmente?");
+			anoAtual = scan.nextInt();
+			
+			System.out.println("Em que ano você nasceu?");
+			anoNasc = scan.nextInt();
+			
+			int idade = anoAtual - anoNasc;
+			
+			System.out.println("Sua idade é, desconsiderando o dia e o mês: " + idade);
 			
 		} catch (Exception e) {
 			System.out.println("Formato incorreto de número! Tente novamente!");
